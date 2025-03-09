c7f11bedd651d1bfbe7d450002d7f9728d1ab4b7
chiennc4805 <chiennguyen5981@gmail.com>
2025-03-10 00:30:45 +0700

    video 53

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,42 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Trả về String là trả về tên file
+    @RequestMapping("/")
+    public String getHomePage() {
+        String test = this.userService.handleHello();
+        return "eric.html";
+    }
+}
+
+// @RestController
+// public class UserController {
+
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("/")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
